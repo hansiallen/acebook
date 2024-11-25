6946823c969e846bedb047d3f1266b1e07f9d851
God'smercy Oluwanusin <gmercyoluwanusin@gmail.com>
2024-11-25 17:23:28 +0000

    updating post model and test to include new variables

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -1,25 +1,35 @@
 package com.makersacademy.acebook.model;
 
 import jakarta.persistence.*;
-
+import java.time.LocalDateTime;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
+
+@Getter
+@Setter
 @Data
 @Entity
+@NoArgsConstructor
 @Table(name = "POSTS")
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String user_id;
     private String content;
+    private String friends_only;
+    private LocalDateTime timestamp;
 
-    public Post() {}
 
-    public Post(String content) {
+    public Post(String user_id, String content, String friends_only, LocalDateTime timestamp) {
+        this.user_id = user_id;
         this.content = content;
+        this.friends_only = friends_only;
+        this.timestamp = timestamp;
     }
-    public String getContent() { return this.content; }
-    public void setContent(String content) { this.content = content; }
 
 }
--- src/test/java/com/makersacademy/acebook/model/PostTest.java
+++ src/test/java/com/makersacademy/acebook/model/PostTest.java
@@ -5,9 +5,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 public class PostTest {
 
-	private Post post = new Post("hello");
+	private Post post = new Post("user_1", "hello", "friends_only", LocalDateTime.of(2024, 12, 25, 10, 30));
 
 	@Test
 	public void postHasContent() {
