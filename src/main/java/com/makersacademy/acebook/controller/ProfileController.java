dc847516abc7d2c0f9113ecc85da05d10ce98c0a
Johannes Allen <hansiallen@gmail.com>
2024-12-03 15:08:59 +0000

    Rebased files

--- src/main/java/com/makersacademy/acebook/controller/ProfileController.java
+++ src/main/java/com/makersacademy/acebook/controller/ProfileController.java
@@ -1,4 +1,50 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.dto.CommentsHandler;
+import com.makersacademy.acebook.dto.LikesHandler;
+import com.makersacademy.acebook.dto.PostWithData;
+import com.makersacademy.acebook.model.Comment;
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.repository.CommentRepository;
+import com.makersacademy.acebook.repository.PostRepository;
+import com.makersacademy.acebook.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDateTime;
+
+@Controller
 public class ProfileController {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    private String getCurrentUser() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    @GetMapping("/profile/{user_id}")
+    public String index(Model model, @PathVariable Long user_id) {
+        String auth0_id = userRepository.findById(user_id).get().getAuth0Id();
+        Iterable<PostWithData> posts = postRepository.findAllWithData(auth0_id);
+        model.addAttribute("posts", posts);
+        String currentUser = getCurrentUser();
+        model.addAttribute("posts", posts);
+        model.addAttribute("post", new Post());
+        model.addAttribute("comment", new Comment());
+        model.addAttribute("currentUser", currentUser);
+        model.addAttribute("currentTime", LocalDateTime.now());
+        model.addAttribute("likesHandler", new LikesHandler(userRepository, currentUser));
+        model.addAttribute("commentsHandler", new CommentsHandler(commentRepository, currentUser));
+        return "profile/index";
+    }
 }
--- src/main/java/com/makersacademy/acebook/model/Profile.java
+++ src/main/java/com/makersacademy/acebook/model/Profile.java
@@ -1,4 +1,49 @@
 package com.makersacademy.acebook.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "PROFILE_PAGES")
 public class Profile {
+    @Id
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String profilePictureURL;
+    private String bio;
+    private String location;
+    private String websiteURL;
+    private boolean isPublic;
+
+    public Profile(Long userId, String firstName, String lastName, String profilePictureURL, String bio, String location, String websiteURL, boolean isPublic) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profilePictureURL = profilePictureURL;
+        this.bio = bio;
+        this.location = location;
+        this.websiteURL = websiteURL;
+        this.isPublic = isPublic;
+    }
+
+    public Profile() {
+
+    }
+
+    public Long  getUserId() {return this.userId;}
+    public void setUserId(Long userId) {this.userId = userId;}
+    public String getFirstName() {return this.firstName;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public String getLastName() {return this.lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    public String getProfilePictureURL() {return this.profilePictureURL;}
+    public void setProfilePictureURL(String profilePictureURL) { this.profilePictureURL = profilePictureURL;}
+    public String getBio() {return this.bio;}
+    public void setBio(String bio) {this.bio = bio;}
+    public String getLocation() {return this.location;}
+    public void setLocation(String location) {this.location = location;}
+    public String getWebsiteURL() {return this.websiteURL;}
+    public void setWebsiteURL(String websiteURL) {this.websiteURL = websiteURL;}
+    public boolean getIsPublic() {return this.isPublic;}
+    public void setIsPublic(boolean isPublic) {this.isPublic = isPublic;}
 }
