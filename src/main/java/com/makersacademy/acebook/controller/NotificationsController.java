05a570c440f0fb0d5dc1a4ba444e1acac1f39a7b
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-12-03 15:30:15 +0000

    get mapping

---
+++ src/main/java/com/makersacademy/acebook/controller/NotificationsController.java
@@ -0,0 +1,31 @@
+package com.makersacademy.acebook.controller;
+
+import com.makersacademy.acebook.dto.LikesHandler;
+import com.makersacademy.acebook.dto.PostWithData;
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.repository.LikeRepository;
+import com.makersacademy.acebook.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+public class NotificationsController {
+
+        @Autowired
+        NotificationRepository repository;
+
+    @GetMapping("/notifications")
+    public String index(Model model) {
+//        List<PostWithData> notifications = repository.findAllWithData(currentUser);
+//        posts.forEach(post -> getRepliesAndCommentCount(post, post, currentUser));
+
+//        model.addAttribute("notifications", notifications);
+
+        return "notifications/index";
+    }
+}
