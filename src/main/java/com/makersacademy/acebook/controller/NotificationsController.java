0bf751ff53f3e6af1998cc721a058fef52d49519
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-12-03 17:35:58 +0000

    makes changes to templates as well as almost fixes controller

--- src/main/java/com/makersacademy/acebook/controller/NotificationsController.java
+++ src/main/java/com/makersacademy/acebook/controller/NotificationsController.java
@@ -1,9 +1,6 @@
 package com.makersacademy.acebook.controller;
 
-import com.makersacademy.acebook.dto.LikesHandler;
-import com.makersacademy.acebook.dto.PostWithData;
-import com.makersacademy.acebook.model.Post;
-import com.makersacademy.acebook.repository.LikeRepository;
+import com.makersacademy.acebook.model.Notification;
 import com.makersacademy.acebook.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,8 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDateTime;
 import java.util.List;
+
 
 @Controller
 public class NotificationsController {
@@ -26,10 +23,10 @@ public class NotificationsController {
 
     @GetMapping("/notifications")
     public String index(Model model) {
-//        List<Notification> notifications = repository.findAllWithData(currentUser);
-
+//        List<Notification> notifications = repository.findByUserId(getCurrentUser());
 //        model.addAttribute("notifications", notifications);
 
         return "notifications/index";
     }
+
 }
