8302142b4a314bb47ed86d6b54706c3bee7d7de1
Joseph-github1 <jpfigs200@gmail.com>
2024-12-03 17:12:15 +0000

    get all noti

--- src/main/java/com/makersacademy/acebook/repository/NotificationRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/NotificationRepository.java
@@ -1,7 +1,18 @@
 package com.makersacademy.acebook.repository;
 
+
 import com.makersacademy.acebook.model.Notification;
+
 import org.springframework.data.repository.CrudRepository;
 
+
+import java.util.List;
+
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
+
+
+
+    List<Notification> findByUserId(String userId);
+
 }
+
