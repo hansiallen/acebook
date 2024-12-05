a79da7501462bc34cc3f70f744d7385487aa7b58
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-12-05 13:01:52 +0000

    friends load in sidebar

--- src/main/java/com/makersacademy/acebook/controller/FriendsController.java
+++ src/main/java/com/makersacademy/acebook/controller/FriendsController.java
@@ -1,31 +1,48 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.dto.FriendWithData;
 import com.makersacademy.acebook.model.Friend;
+import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.FriendRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class FriendsController {
     @Autowired
     FriendRepository repository;
 
-    @GetMapping("/friends")
-    public String index(Model model) {
-        return "friends/index";
+    private String getCurrentUser() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 
-    @GetMapping("/friends-test")
-    @ResponseBody
-    public List<Friend> test(Model model) {
-        String userA = "auth0|6748e51bcc1b98c050fe67e2";
-        String userB = "auth0|674634d86a29392e14307373";
-        List<Friend> commonFriends = repository.findCommonFriends(userA, userB);
-        return commonFriends;
+    private List<FriendWithData> getFriends() {
+        List<User> friends = repository.findFriends(getCurrentUser());
+        List<FriendWithData> friendsWithData = new ArrayList<>();
+
+        for (User friend : friends) {
+            List<User> friendsOfFriend = repository.findFriends(friend.getAuth0Id());
+            List<User> mutualFriends = friends.stream()
+                    .filter(friendsOfFriend::contains)
+                    .collect(Collectors.toList());
+
+            friendsWithData.add(new FriendWithData(friend.getId(), friend.getAuth0Id(), friend.getNickname(), friend.getLastLogin(), mutualFriends));
+        }
+
+        return friendsWithData;
+    }
+
+    @GetMapping("/friends")
+    public String index(Model model) {
+        model.addAttribute("friends", getFriends());
+        return "friends/index";
     }
 }
--- src/main/java/com/makersacademy/acebook/repository/FriendRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/FriendRepository.java
@@ -1,5 +1,7 @@
 package com.makersacademy.acebook.repository;
 
+import com.makersacademy.acebook.dto.FriendWithData;
+import com.makersacademy.acebook.model.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import com.makersacademy.acebook.model.Friend;
@@ -9,9 +11,8 @@ import java.util.List;
 
 
 public interface FriendRepository extends CrudRepository<Friend, Long> {
-    //To check for mutual friends
-    @Query("SELECT f FROM Friend f WHERE (f.user_a = :user_a " +
-            "AND f.user_b IN (SELECT f2.user_b FROM Friend f2 WHERE f2.user_a = :user_b)) " +
-            "OR (f.user_b = :user_a AND f.user_a IN (SELECT f2.user_a FROM Friend f2 WHERE f2.user_b = :user_b))")
-    List<Friend> findCommonFriends(@Param("user_a") String user_a, @Param("user_b") String user_b );
-        }
+    @Query("SELECT u FROM User u " +
+            "JOIN Friend f ON (u.auth0Id = f.userA OR u.auth0Id = f.userB) " +
+            "WHERE (f.userA = :auth0Id OR f.userB = :auth0Id) AND u.auth0Id != :auth0Id")
+    List<User> findFriends(@Param("auth0Id") String auth0Id);
+}
