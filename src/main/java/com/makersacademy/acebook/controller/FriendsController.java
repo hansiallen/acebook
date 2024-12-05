ce1f42b16900fc151de864c3aac2bec6a7c52d47
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-12-05 10:33:00 +0000

    basic mutual friends functionality

--- src/main/java/com/makersacademy/acebook/controller/FriendsController.java
+++ src/main/java/com/makersacademy/acebook/controller/FriendsController.java
@@ -1,13 +1,31 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Friend;
+import com.makersacademy.acebook.repository.FriendRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class FriendsController {
+    @Autowired
+    FriendRepository repository;
+
     @GetMapping("/friends")
     public String index(Model model) {
         return "friends/index";
+    }
+
+    @GetMapping("/friends-test")
+    @ResponseBody
+    public List<Friend> test(Model model) {
+        String userA = "auth0|6748e51bcc1b98c050fe67e2";
+        String userB = "auth0|674634d86a29392e14307373";
+        List<Friend> commonFriends = repository.findCommonFriends(userA, userB);
+        return commonFriends;
     }
 }
--- src/main/java/com/makersacademy/acebook/repository/FriendRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/FriendRepository.java
@@ -9,13 +9,9 @@ import java.util.List;
 
 
 public interface FriendRepository extends CrudRepository<Friend, Long> {
-    //To remove a friend
-    @Query("DELETE FROM Friend f WHERE (f.user_a = :user_a AND f.user_b = :user_b) OR (f.user_a = :user_b AND f.user_b = :user_a)")
-    void deleteFriendship(@Param("user_a") String user_a, @Param("user_b") String user_b);
-
     //To check for mutual friends
     @Query("SELECT f FROM Friend f WHERE (f.user_a = :user_a " +
             "AND f.user_b IN (SELECT f2.user_b FROM Friend f2 WHERE f2.user_a = :user_b)) " +
             "OR (f.user_b = :user_a AND f.user_a IN (SELECT f2.user_a FROM Friend f2 WHERE f2.user_b = :user_b))")
-    List<FriendRepository> findCommonFriends(@Param("user_a") String user_a, @Param("user_b") String user_b );
+    List<Friend> findCommonFriends(@Param("user_a") String user_a, @Param("user_b") String user_b );
         }
