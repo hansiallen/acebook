ba78d81dc4466174589bb7198927f53a174d028f
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-12-06 10:33:02 +0000

    added notifications when you get sent a freind request

--- src/main/java/com/makersacademy/acebook/controller/FriendRequestsController.java
+++ src/main/java/com/makersacademy/acebook/controller/FriendRequestsController.java
@@ -1,8 +1,12 @@
 package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.FriendRequest;
+import com.makersacademy.acebook.model.Notification;
 import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.FriendRequestRepository;
+import com.makersacademy.acebook.repository.NotificationRepository;
+import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,10 +17,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Controller
 public class FriendRequestsController {
     @Autowired
     FriendRequestRepository repository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private NotificationRepository notificationRepository;
+
 
     private String getCurrentUser() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
@@ -34,6 +46,18 @@ public class FriendRequestsController {
     @PostMapping("/friend-requests")
     public RedirectView create(@ModelAttribute FriendRequest friendRequest) {
         repository.save(friendRequest);
+
+        //sends notification
+
+        String getCurrentUser=SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByAuth0Id(getCurrentUser);
+        Optional<User> receiver= userRepository.findUserByAuth0Id(friendRequest.getRequestedUser());
+        if (receiver.isPresent()){
+            String ownerId = receiver.get().getAuth0Id();
+            Notification newFriendRequest = new Notification(ownerId, user.getNickname() +" sent you a friend request", LocalDateTime.now(),"/friends");
+            notificationRepository.save(newFriendRequest);
+        }
+
         return new RedirectView("/friends");
     }
 }
